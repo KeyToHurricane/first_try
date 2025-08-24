8b1242c96491ce555adaa720211559846fe00720
Key <ruru008832@gmail.com>
2025-08-24 19:03:02 +0300

    Add buttons_add shrifts_work with css butons

---
+++ solotry/src/Main.js
@@ -0,0 +1,19 @@
+function Main() {
+  return (
+    <div className="Main">
+      <header>
+        <div className="menu">
+          <a>About me</a>
+          <a>Services</a>
+          <a>Portfolio</a>
+          <a>Reviews</a>
+          <a>Garanties</a>
+        </div>
+
+        <button className="btn">Contact</button>
+      </header>
+    </div>
+  );
+}
+
+export default Main;
