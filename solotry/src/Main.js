85baf5a5c34504292d534bd823d0c47626a6832f
Key <ruru008832@gmail.com>
2025-08-27 20:02:34 +0300

    Added block contact with functions button contact need fix buttons with links

--- solotry/src/Main.js
+++ solotry/src/Main.js
@@ -1,4 +1,17 @@
-function Main() {
+import React, {useState} from 'react';
+import ModalWindow from './ModalWindow';
+
+const Main = () => {
+  const [showModal, setShowModal] = useState(false);
+
+  const handleOpenModal = () => {
+    setShowModal(true);  
+  }
+
+  const handleCloseModal = () => {
+    setShowModal(false);
+  }
+
   return (
     <div className="Main">
       <header>
@@ -10,10 +23,25 @@ function Main() {
           <a>Garanties</a>
         </div>
 
-        <button className="btn">Contact</button>
+        <button onClick={handleOpenModal} className="btn">Contact</button>
+        <ModalWindow show={showModal} onClose={handleCloseModal}>
+          <h2 style={{ color: "#4824ff", fontSize: "40px"}}>Way to Contact</h2>
+          <p style={{fontSize: "22px"}}>You can contact with me on Telegram<br/>or instagram 👇</p>
+        </ModalWindow>
+
+        <a href="https://t.me/+cMXstvUN1Yg5MmQ6" target="_blank"
+        className="icon telegram"/>
+        <a href="https://www.instagram.com/kostya_zagoruyko?igsh=MWV6N3J4YjdtM3N0Yg%3D%3D&utm_source=qr" target="_blank"
+        className="icon instagram"/>
+
+        <div className="switch">
+          <div className="theme light"></div>
+        </div>
       </header>
     </div>
   );
 }
+
+
 
 export default Main;
